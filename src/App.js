f2adff6dff70242bbba5ead87b7a8e08863921eb
JC <nchan009@gmail.com>
2022-07-20 20:39:26 +1000

    use api to retrieve data from person service

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import './index.css'
 
 const JsonBody1 = (props) => {
   return(
     <>
-      <p>"{props.name}": "<a href={props.link} rel="noreferrer" target="_blank">{props.value}</a>",</p>
+      "{props.name}": "<a href={props.link} rel="noreferrer" target="_blank">{props.value}</a>"
     </>
   )
 }
 
-const JsonBody2 = (props) => {
-  return(
+const JsonBodyObject = (props) => {
+  return (
     <>
-      <p>"{props.name}": "{props.value}"</p>
+      <p>
+        "{props.name}": "{props.list.message}"
+      </p>
     </>
-  )
-}
+  );
+};
 
-function JsonArrayString(props) {
+function JsonArrayObject(props) {
   let content = "";
 
   for (var i = 0; i < props.length; i++) {
-    if (i === props.length - 1) {
-      content += props[i]
-    } else {
-      content += props[i] + "\", \"" 
+    if (props.hasOwnProperty(i)) {
+      if (i === props.length - 1) {
+        content += props[i].name;
+      } else {
+        content += props[i].name + '", "';
+      }
+    }
+  }
+
+  return content;
+}
+
+function JsonArrayObject2(props) {
+  let content = "";
+
+  for (var i = 0; i < props.length; i++) {
+    if (props.hasOwnProperty(i)) {
+      if (i === props.length - 1) {
+        content += props[i].message;
+      } else {
+        content += props[i].message + '", "';
+      }
     }
   }
 
@@ -32,61 +52,128 @@ function JsonArrayString(props) {
 }
 
 const JsonBodyArray = (props) => {
-  return(
+  return (
     <>
-      <p>"{props.name}": ["{JsonArrayString(props.list)}"],</p>
+      <p>
+        "{props.name}": ["{JsonArrayObject(props.list)}"]
+      </p>
     </>
-  )
+  );
 }
+
+const JsonBodyArray2 = (props) => {
+  return (
+    <>
+      <p>
+        "{props.name}": ["{JsonArrayObject2(props.list)}"]
+      </p>
+    </>
+  );
+};
 
 const App = () => {
 
-  const skills = ["java", "groovy", "rust", "bash"];
-  const framework = ["spring-boot", "apache-camel", "quarkus"];
-  const cicd = ["jenkins", "github-actions"];
-  const containers = ["docker", "kubernetes", "openshift"];
-  const monitoring = ["prometheus", "grafana", "splunk"];
-  const messaging_platform = ["amq", "ibm mq"];
+  const [persons, setPerson] = useState();
 
-  const hobbies = ["k3s", "raspi4", "💤", "🏃‍♂️", "📖"];
-  const learn = ["go", "rust", "react"];
+  const personUrl = "http://person-service.jc-dev.svc";
 
+  // Function to collect data
+  const getPerson = async () => {
+    const response = await fetch(
+      personUrl + "/api/v1/person/JC"
+    ).then((response) => response.json());
 
-  return(
-  <>
-      <div class="header"><p>👋👋</p></div>
-      <p>"JC": &#123;</p>
-      <div class="json-parent"><p>"socials": &#123;</p></div>
-      <div class="json-child">
-        <JsonBody1 name="github" link="https://github.com/shin-san" value="shin-san"/>
-        <JsonBody1 name="linkedin" link="https://www.linkedin.com/in/naythanc" value="naythanc"/>
-        <p>"contact": "<a href="mailto:nchan009@gmail.com">nchan009@gmail.com</a>"</p>
-      </div>
-      <div class="json-parent"><p>&#125;</p></div>
-      <p>&#125;,</p>
-      <div class="json-parent"><p>"skills": &#123;</p></div>
-      <div class="json-child">
-        <JsonBodyArray name="language" list={skills}/>
-        <JsonBodyArray name="framework" list={framework}/>
-        <JsonBodyArray name="cicd" list={cicd}/>
-        <JsonBodyArray name="container" list={containers}/>
-        <JsonBodyArray name="monitoring" list={monitoring}/>
-        <JsonBodyArray name="messaging_platform" list={messaging_platform}/>
-        <JsonBody2 name="security" value ="oauth"/>
-      </div>
-      <div class="json-parent"><p>&#125;</p></div>
-      <p>&#125;</p>
-      <div class="json-parent"><p>"misc": &#123;</p></div>
-      <div class="json-child">
-        <JsonBodyArray name="hobbies" list={hobbies}/>
-        <JsonBodyArray name="learn" list={learn}/>
-        <JsonBody2 name="uh" value ="dunno what else to add"/>
-      </div>
-      <div class="json-parent"><p>&#125;</p></div>
-      <p>&#125;</p>
-      <div class="footer">by: yours truly</div>
-  </>
-  )
+    setPerson(response);
+  };
+
+  useEffect(() => {
+    getPerson();
+  }, []);
+
+  console.log(persons);
+
+  return (
+    <>
+      {persons && 
+        persons.map((person) => {
+        let socials = person.socials.map((social) => {
+          return (
+            <>
+              <p className="json-child">
+                <JsonBody1
+                  name={social.name}
+                  link={social.url}
+                  value={social.social_value}
+                />
+              </p>
+            </>
+          );
+        });
+        let skills = person.skills.map((skills) => {
+          return (
+            <>
+              <div className="json-child">
+                <JsonBodyArray name="language" list={skills.languages} />
+                <JsonBodyArray name="framework" list={skills.framework} />
+                <JsonBodyArray name="cicd" list={skills.cicd} />
+                <JsonBodyArray name="container" list={skills.container} />
+                <JsonBodyArray name="monitoring" list={skills.monitoring} />
+                <JsonBodyArray
+                  name="messaging_platform"
+                  list={skills.messaging_platform}
+                />
+                <JsonBodyArray name="security" list={skills.security} />
+              </div>
+            </>
+          );
+        });
+        let misc = person.misc.map((misc) => {
+          return (
+            <>
+              <div className="json-child">
+                <JsonBodyArray name="hobbies" list={misc.hobbies} />
+                <JsonBodyArray2 name="learn" list={misc.learn} />
+                <JsonBodyObject name="uh" list={misc.uh} />
+              </div>
+            </>
+          );
+        });
+        return (
+          <>
+            <div className="header">
+              <p>👋👋</p>
+            </div>
+            <div>"{person.name}": &#123;</div>
+            <div className="json-parent">
+              <p>"socials": &#123;</p>
+            </div>
+            <div>{socials}</div>
+            <div className="json-parent">
+              <p>&#125;</p>
+            </div>
+            <p>&#125;</p>
+            <div className="json-parent">
+              <p>"skills": &#123;</p>
+            </div>
+            {skills}
+            <div className="json-parent">
+              <p>&#125;</p>
+            </div>
+            <p>&#125;</p>
+            <div className="json-parent">
+              <p>"misc": &#123;</p>
+            </div>
+            {misc}
+            <div className="json-parent">
+              <p>&#125;</p>
+            </div>
+            <p>&#125;</p>
+          </>
+        );
+      })}
+      <div className="footer">by: JC</div>
+    </>
+  );
 }
 
 export default App
